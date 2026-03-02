b73717c9ded5fb750d43553fc661d9ed1cfbc592
minminpinwini <ppinedag2400@alumno.ipn.mx>
2026-03-02 17:54:18 -0600

    mi primer hola mundo

---
+++ 01HolaMundo.java
@@ -0,0 +1,17 @@
+//para un comentario de una sola linea 
+
+    /*java es un lenguaje de programacion multi paradigama signifca
+    que se adapta a las necesidades del modelo o arquitectura
+    deprogramacion Java originalmente fue pensado paraprogramar electrodomesticos, este lenguaje utiliza auna maquina virtual de java. Esa maquina virtual se encarga de transformar el codigo de maquina en las instrucciones definidas por el programador y viceversa. Cuando programamos en JAVA debemos de compilar el codigo */
+
+class HolaMundo01 {
+    //Este es el cuerpo de la clase
+    //aqui se definen las variables
+    //aqui definimos los comportamientos (metodos)
+    public static void main(String[] args){
+        System.out.print("Hola Mundo mundial");
+
+
+    }
+
+}
