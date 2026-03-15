4ec81254be8d9e5ec16bfa0942d8adeab7cb0881
minminpinwini <ppinedag2400@alumno.ipn.mx>
2026-03-15 01:54:38 -0600

    ejercicios del 1 al 6

--- 03/EstructuraDeDatos.java
+++ 03/EstructuraDeDatos.java
@@ -1,6 +1,6 @@
 /*
 Vamos a crear 14 programas dentro de un menu gigante para poner a prueba sus conocimientos de algoritmia 
-1- Desarrollar un prgrama para calcular el boo de un descuento por edad
+1- Desarrollar un prgrama para calcular el bono de un descuento por edad
 2- Convertir numeros de decimales a binarios 
 3-Convertir temperaturas entre los principales grados c -> F y K 
 4- Realizar un nuevo programa para contar numeros de positivos a negativos de una serie de numeros
@@ -57,85 +57,238 @@ public class EstructuraDeDatos{
     //Ahora tengo que evaluar entrada
     switch (opcion) {
         case 1:
-            
+            System.out.println("Ingrese su edad");
+            int edad = entrada.nextInt();
+
+            float descuento = 0;
+
+            if(edad > 0){
+
+                if(edad >= 60){
+                    descuento = 0.30f;
+                    System.out.println("Tiene un descuento del 30%");
+                }else if (edad >=30){
+                    descuento = 0.20f;
+                    System.out.println("Tiene un descuento del 20%");
+                }else if(edad >= 18){
+                    descuento = 0.10f;
+                    System.out.println("Tiene un descuento del 10%");
+
+                }else{
+                    descuento = 0;
+                    System.out.println("No tiene descuento");
+                }
+            }else{
+                System.out.println("Ingrese una edad valida");
+            }
+
         break;
         case 2:
-            // Convertir decimal a binario
-         System.out.println("Ingrese un numero decimal positivo que desee convertir a binario");
+            //convertir un numero decimal a binario
+            System.out.println("Ingrese un numero positivo entero que se desee convertir a binario");
             int numbinario;
-            String guardarbinario = "";
+            String guardarbinario ="";
             numbinario = entrada.nextInt();
 
-        if (numbinario > 0) {
-        while (numbinario > 0) {
+            if(numbinario > 0){
+                //realizamos el mod 2
+                while(numbinario > 0){
+                    if(numbinario%2 == 0){
+                        guardarbinario = "0" + guardarbinario;
 
-        if (numbinario % 2 == 0) {
-            guardarbinario = "0" + guardarbinario;
-         } else {
-         guardarbinario = "1" + guardarbinario;
-         }
+                    }else{
+                        guardarbinario = "1" + guardarbinario;
 
-         numbinario = numbinario / 2;
-        }
+                    }
+                    numbinario = (int)numbinario/2;
+                }
 
-        } else if (numbinario == 0) {
-         guardarbinario = "0";
-
-        } else {
-
-        guardarbinario = "No se puede convertir ese numero, solo se aceptan positivos";
-
-         }
-
-         System.out.println("El numero en binario es: " + guardarbinario);
+            }else if(numbinario == 0){
+                
+                guardarbinario = "0";
+            }else{
+               //aqui no se puede 
+               guardarbinario = "No se puede convertir ese numero, solo acepta positivos";
+            }
+            System.out.println("El numero convertido a binario es: " + guardarbinario);
         break;
         case 3:
             
+        System.out.println("Seleccione el tipo de conversion ");
+        System.out.println("1. C => F");
+        System.out.println("2. C => K");
+
+        int opcionn = 0;
+        opcionn = entrada.nextInt();
+
+        System.out.println("ingrese la temperatura");
+        float temperatura = 0;
+        temperatura = entrada.nextFloat();
+
+        float resultadoo = 0;
+        if(opcionn == 1){
+            resultadoo = (temperatura * 9/5) + 32;
+        System.out.println("La temperatura en F es: " + resultadoo);
+
+        }else if(opcionn == 2){
+            resultadoo = temperatura + 273.15f;
+        System.out.println("L temperatura en K es: " + resultadoo);
+
+        }else{
+        System.out.println("Opcion Invalida :c");
+
+        }
+
+
+
         break;
         case 4:
+        System.out.println("Cuantos numeros desea ingresar");
+        int cantidadnumeros = 0;
+        cantidadnumeros = entrada.nextInt();
+
+        int positivos = 0;
+        int negativos = 0;
+
+        if(cantidadnumeros >0){
+
+        for(int i = 1; i <= cantidadnumeros; i++){
+            System.out.println("Ingrese un numero");
+            int numero = 0;
+            numero = entrada.nextInt();
+
+            if(numero > 0){
+                positivos = positivos + 1;
+             }else if(numero <0){
+                negativos = negativos + 1;
+
+            
+             }
+
+        }
+        System.out.println("Numeros positivos: " +  positivos);
+        System.out.println("Numeros negativos: " + negativos);
+
+    }else{
+        System.out.println("Ingrese solo cantidades positivas");
+    }
+
         
         break;
         case 5:
-            System.out.println("Buenvenido a esta hermosa tiendita linada y kawaii");
-            System.out.println("Por faavor ingrese cuantos elementos va a comprar");
-            int elementosproducto = 0;
+        System.out.println("Bienvenido a esta hermosa tiendita linda y kawaii");
+        System.out.println("Por favor ingrese cuantos elementos va a comprar");
+        int elementosproducto = 0;
+        elementosproducto = entrada.nextInt();
+        if( elementosproducto > 0){
 
-            elementosproducto = entrada.nextInt();
-            if( elementosproducto > 0){
+            float compra=0;
 
-                float compra=0;
+            for(int i = 1; i<= elementosproducto; i++){
+                System.out.println("Ingresa el nombre del producto");
+                String nombreproducto="";
+                nombreproducto = entrada.next();
+                System.out.println("Ingrese el precio");
+                float precio=0;
+                precio = entrada.nextFloat();
+                float resultado;
+                System.out.println("Ingrese la cantidad de producto");
+                int cantidad=0;
+                cantidad = entrada.nextInt();
+                resultado = precio * cantidad;
+                
+                compra = resultado + compra;
 
-                for (int i = 1; i<=
-                elementosproducto; i++
-                ){
-                    System.out.println("Ingresa el nombre del producto");
-                    String nombreproducto;
-                    nombreproducto = entrada.next();
 
-                    System.out.println("Ingrese el precio");
-                    float precio=0;
-                    precio = entrada.nextFloat();
-                    float resultado;
-
-                    System.out.println("Ingrese el cantidad");
-                    int cantidad=0;
-                    precio = entrada.nextInt();
-                    
-                    resultado = precio * cantidad;
-                    
-                    compra = resultado + compra;
-                    
-                    
-                }
-                System.out.println("El total de  la compra es: " + compra);
-
-            }else {
-                System.out.println("Ingrese solo valores positivos");
             }
-            
+            System.out.println("El total de la compra es: " + compra);
+
+        }else{
+            System.out.println("ingrese solo cantidades positivas");
+        }
+        
         break;
         case 6:
-            
+        System.out.println("Seleccione la figura");
+        System.out.println("1. Cuadrado");
+        System.out.println("2. rectangulo"); 
+        System.out.println("3. triangulo"); 
+        System.out.println("4. Circulo"); 
+        System.out.println("5. Pentagono"); 
+
+        int figura = 0;
+        figura = entrada.nextInt();
+
+        float area = 0;
+        float perimetro = 0;
+
+        if(figura == 1){
+            System.out.println("Ingresa el lado");
+            float lado = 0;
+            lado = entrada.nextFloat();
+
+            area = lado * lado;
+            perimetro = lado * 4;
+
+             }else if(figura == 2){
+
+                System.out.println("Ingrese base");
+                float base = 0;
+                base = entrada.nextFloat();
+
+                System.out.println("Ingrese altura");
+                float altura = 0;
+                altura = entrada.nextFloat();
+
+                area = base * altura;
+                perimetro = 2 + (base + altura);
+
+              }else if (figura == 3){
+                System.out.println("ingrese la base ");
+                float base = 0;
+                base = entrada.nextFloat();
+
+                System.out.println("Ingrese altura");
+                float altura = 0;
+                altura = entrada.nextFloat();
+
+                System.out.println("Ingresa lado 2 ");
+                float lado2 = 0;
+                lado2 = entrada.nextInt();
+
+                System.out.println("Ingresa lado 3");
+                float lado3 = 0;
+                lado3 = entrada.nextFloat();
+
+                area = (base * altura) /2;
+                perimetro = base + lado2 + lado3;
+            }else if (figura == 4){
+
+                System.out.println("Ingrese el radio");
+                float radio = 0;
+                radio = entrada.nextFloat();
+
+                area = 3.1416f * radio * radio;
+                perimetro = 2 * 3.1416f * radio;
+
+            }else if (figura == 5) {
+                System.out.println("Ingrese el lado");
+                float lado = 0;
+                lado = entrada.nextFloat();
+
+                System.out.println("Ingrese el apotema");
+                float apotema = 0;
+                apotema = entrada.nextFloat();
+
+                perimetro = lado * 5;
+                area = (perimetro * apotema) / 2;
+            }
+            System.out.println("el area es:" + area);
+            System.out.println("el perimetro es: " + perimetro);
+
+        
+
+ 
         break;
         case 7:
             
@@ -160,13 +313,14 @@ public class EstructuraDeDatos{
         break;
         
         default:
-            throw new AssertionError();
+          
 
 
         }
-        System.out.println("Deseas repetir el programa, escribe S o s para si ");
-        letrapararepetir = entrada.next().charAt(0);
-    }while(letrapararepetir != 's' || letrapararepetir == 'S');
+        System.out.println("Deseas repetir el programa escribe s o S para si");
+        letrapararepetir = entrada.next().charAt(0);  
+        }while(letrapararepetir != 's' || letrapararepetir == 'S');    
+
 
 
     }
